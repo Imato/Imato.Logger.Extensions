36feeb3c0ef52d7064bf0879fbfd3948af1dfe85
imato <alexey.varentsov@gmail.com>
2024-05-15 10:06:17 +0300

    ver 1.0.0

---
+++ Imato.Logger.Extensions/Duration.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Imato.Logger.Extensions
+{
+    public static class Duration
+    {
+        /// <summary>
+        /// Log task task duration. Create warning if maxDuration exceeded
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="logger"></param>
+        /// <param name="task"></param>
+        /// <param name="taskName">Log message</param>
+        /// <param name="maxDuration"></param>
+        /// <returns></returns>
+        public static async Task<T> LogDuration<T>(this ILogger logger,
+            Func<Task<T>> task,
+            string taskName = "",
+            int maxDuration = 10_000)
+        {
+            var now = DateTime.UtcNow;
+            logger?.LogInformation(() => $"Start {taskName}");
+            var result = await task();
+
+            var time = (DateTime.UtcNow - now).TotalMilliseconds;
+            if (time > maxDuration)
+            {
+                logger?.LogWarning(() => $"{taskName} max duration {maxDuration} exceeded - {time}");
+            }
+
+            logger?.LogInformation(() => $"End {taskName}. Duration: {time}");
+
+            return result;
+        }
+
+        /// <summary>
+        /// Log task task duration. Create warning if maxDuration exceeded
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="task"></param>
+        /// <param name="taskName">Log message</param>
+        /// <param name="maxDuration"></param>
+        /// <returns></returns>
+        public static async Task LogDuration(this ILogger logger,
+            Func<Task> task,
+            string taskName = "",
+            int maxDuration = 10_000)
+        {
+            await LogDuration(logger,
+                async () =>
+                {
+                    await task();
+                    return Task.FromResult(true);
+                },
+                taskName, maxDuration);
+        }
+    }
+}
---
+++ Imato.Logger.Extensions/Log.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace Imato.Logger.Extensions
+{
+    public static class Log
+    {
+        public static void LogTrace(this ILogger logger, Func<string> messageFactory)
+        {
+            if (logger.IsEnabled(LogLevel.Trace))
+            {
+                logger.LogTrace(messageFactory());
+            }
+        }
+
+        public static void LogDebug(this ILogger logger, Func<string> messageFactory)
+        {
+            if (logger.IsEnabled(LogLevel.Debug))
+            {
+                logger.LogDebug(messageFactory());
+            }
+        }
+
+        public static void LogInformation(this ILogger logger, Func<string> messageFactory)
+        {
+            if (logger.IsEnabled(LogLevel.Information))
+            {
+                logger.LogInformation(messageFactory());
+            }
+        }
+
+        public static void LogWarning(this ILogger logger, Func<string> messageFactory)
+        {
+            if (logger.IsEnabled(LogLevel.Warning))
+            {
+                logger.LogWarning(messageFactory());
+            }
+        }
+
+        public static void LogError(this ILogger logger, Func<string> messageFactory)
+        {
+            if (logger.IsEnabled(LogLevel.Error))
+            {
+                logger.LogError(messageFactory());
+            }
+        }
+
+        public static void LogCritical(this ILogger logger, Func<string> messageFactory)
+        {
+            if (logger.IsEnabled(LogLevel.Critical))
+            {
+                logger.LogCritical(messageFactory());
+            }
+        }
+
+        public static void LogTrace(this ILogger logger, Exception exception, Func<string>? messageFactory)
+        {
+            if (logger.IsEnabled(LogLevel.Trace))
+            {
+                logger.LogTrace(exception, messageFactory != null ? messageFactory() : null);
+            }
+        }
+
+        public static void LogDebug(this ILogger logger, Exception exception, Func<string>? messageFactory)
+        {
+            if (logger.IsEnabled(LogLevel.Debug))
+            {
+                logger.LogDebug(exception, messageFactory != null ? messageFactory() : null);
+            }
+        }
+
+        public static void LogInformation(this ILogger logger, Exception exception, Func<string>? messageFactory)
+        {
+            if (logger.IsEnabled(LogLevel.Information))
+            {
+                logger.LogInformation(exception, messageFactory != null ? messageFactory() : null);
+            }
+        }
+
+        public static void LogWarning(this ILogger logger, Exception exception, Func<string>? messageFactory)
+        {
+            if (logger.IsEnabled(LogLevel.Warning))
+            {
+                logger.LogWarning(exception, messageFactory != null ? messageFactory() : null);
+            }
+        }
+
+        public static void LogError(this ILogger logger, Exception exception, Func<string>? messageFactory)
+        {
+            if (logger.IsEnabled(LogLevel.Error))
+            {
+                logger.LogError(exception, messageFactory != null ? messageFactory() : null);
+            }
+        }
+
+        public static void LogCritical(this ILogger logger, Exception exception, Func<string>? messageFactory)
+        {
+            if (logger.IsEnabled(LogLevel.Critical))
+            {
+                logger.LogCritical(exception, messageFactory != null ? messageFactory() : null);
+            }
+        }
+    }
+}
