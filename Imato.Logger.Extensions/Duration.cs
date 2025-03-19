5a03292e1482d5ab38577a89a1193a5611d77f30
a.varentsov <a.varentsov@corp.mail.ru>
2025-03-19 20:44:26 +0300

    ver 1.2.3

--- Imato.Logger.Extensions/Duration.cs
+++ Imato.Logger.Extensions/Duration.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace Imato.Logger.Extensions
 {
@@ -20,17 +21,39 @@ namespace Imato.Logger.Extensions
             string taskName = "",
             int maxDuration = 10_000)
         {
-            var now = DateTime.UtcNow;
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
             logger?.LogInformation(() => $"Start {taskName}");
             var result = await task();
 
-            var time = (DateTime.UtcNow - now).TotalMilliseconds;
-            if (time > maxDuration)
+            stopWatch.Stop();
+            if (stopWatch.ElapsedMilliseconds > maxDuration)
             {
-                logger?.LogWarning(() => $"{taskName} max duration {maxDuration} exceeded - {time}");
+                logger?.LogWarning(() => $"{taskName} max duration {maxDuration} ms exceeded - {stopWatch.ElapsedMilliseconds} ms");
             }
 
-            logger?.LogInformation(() => $"End {taskName}. Duration: {time}");
+            logger?.LogInformation(() => $"End {taskName}. Duration: {stopWatch.ElapsedMilliseconds}");
+
+            return result;
+        }
+
+        public static T LogDuration<T>(this ILogger logger,
+            Func<T> task,
+            string taskName = "",
+            int maxDuration = 10_000)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            logger?.LogInformation(() => $"Start {taskName}");
+            var result = task();
+
+            stopWatch.Stop();
+            if (stopWatch.ElapsedMilliseconds > maxDuration)
+            {
+                logger?.LogWarning(() => $"{taskName} max duration {maxDuration} ms exceeded - {stopWatch.ElapsedMilliseconds} ms");
+            }
+
+            logger?.LogInformation(() => $"End {taskName}. Duration: {stopWatch.ElapsedMilliseconds}");
 
             return result;
         }
@@ -53,6 +76,28 @@ namespace Imato.Logger.Extensions
                 {
                     await task();
                     return Task.FromResult(true);
+                },
+                taskName, maxDuration);
+        }
+
+        /// <summary>
+        /// Log task task duration. Create warning if maxDuration exceeded
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="task"></param>
+        /// <param name="taskName">Log message</param>
+        /// <param name="maxDuration">Create warning after</param>
+        /// <returns></returns>
+        public static void LogDuration(this ILogger logger,
+            Action task,
+            string taskName = "",
+            int maxDuration = 10_000)
+        {
+            LogDuration(logger,
+                () =>
+                {
+                    task();
+                    return true;
                 },
                 taskName, maxDuration);
         }
